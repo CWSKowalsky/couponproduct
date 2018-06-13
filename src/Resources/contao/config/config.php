c16f0cfb1ff215c9920efa13f96659af3beaa67a
SKowalsky <skowalsky37@gmail.com>
2018-06-13 10:36:51 +0200

    edited config + dca

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,6 +4,14 @@ use Contao\CoreBundle\ContaoCoreBundle;
 
 $GLOBALS['BE_MOD']['content']['newsletter']['export'] = array('TestBundle\Test', 'exportRecipients');
 
+array_insert($GLOBALS['BE_MOD'], 0, array(
+	'computerworkstestbundle' => array(
+		'coupon_products' => array(
+			'tables' => 'tl_coupon_products'
+        )
+    )
+));
+
 if (TL_MODE == 'BE' && strlen($GLOBALS['BE_MOD']['content']['newsletter']['stylesheet'])) {
     $GLOBALS['TL_CSS'][] = 'bundles/computerworkstest/style.css';
 }
